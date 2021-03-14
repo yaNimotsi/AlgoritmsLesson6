23e9b8aa1b8c556621345b89ac7c66c086fd1e5f
yaNimotsi <yanimotsi@yanmdex.ru>
2021-03-14 18:16:14 +0300

    Add project files.

---
+++ AlgoritmsLesson6Task/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoritmsLesson6Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
